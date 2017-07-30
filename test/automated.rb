9afea6ac54f1a83d3b865205ce2b526c8f4ebf53
ntl <nathanladd+github@gmail.com>
2017-07-30 16:09:58 -0500

    Data type tests can be disabled

--- test/automated.rb
+++ test/automated.rb
@@ -2,7 +2,13 @@ require_relative 'test_init'
 
 require 'test_bench/cli'
 
+exclude_pattern = %r{\/_|sketch|(_init\.rb|_tests\.rb)\z} 
+
+if ENV['SKIP_DATA_TYPE_TESTS'].to_s.match?(%r{\A(?:y|yes|t|true|1)\z}i)
+  exclude_pattern = %r{data_types|#{exclude_pattern}}
+end
+
 TestBench::CLI.(
   tests_directory: './test/automated',
-  exclude_pattern: %r{\/_|sketch|(_init\.rb|_tests\.rb)\z}
+  exclude_pattern: exclude_pattern
 ) or exit 1
